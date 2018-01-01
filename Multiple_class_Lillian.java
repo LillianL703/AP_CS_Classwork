7c480651f3ed533e59ce6b97dca4698473a1a5ee
Lillian L <luozy703@163.com>
2018-01-02 02:03:12 +0800

    Lillian's Christmas HW
    This program is to output festival greetings to users.

---
+++ Multiple_class_Lillian.java
@@ -0,0 +1,30 @@
+/*
+ * This program is to output festival greetings to users.
+ * (Version 1)
+ * Editor: Lillian L. from S3C7
+ * Instructor: Daniel Gunn
+ */
+
+/**
+ *
+ * @author Lillian
+ */
+public class Multiple_class_Lillian {
+        public static void main(String args[]){
+               new B();
+       }
+}
+class A {
+        public A(){
+              System. out.println("Merry Christmas!" );
+       }
+       {System. out.println("(ゝω・)");}
+}
+
+class B  extends  A{
+        public B(){
+              System. out.println("Happy New Year!" );
+       }
+       {System. out.println("(´◡`♥)" );}
+}
+

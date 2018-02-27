d3f4ceeda6832191c319fe903589e8424d96b968
LillianL703 <34665621+LillianL703@users.noreply.github.com>
2018-02-27 09:34:00 +0800

    Create Bicycle.java

---
+++ Bicycle.java
@@ -0,0 +1,12 @@
+public class Bicycle{
+  public static int wheel = 2;
+  public string brand;
+  public double mass;
+  public double speed;
+  
+  public Bicycle(int wheel, string brand, double mass){
+  this.brand = brand;
+  this.mass = mass;
+  this.speed = speed;
+  }
+  
